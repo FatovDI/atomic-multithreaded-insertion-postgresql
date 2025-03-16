a79caa25fbf245bd747f11d94bb3c3d19f5e20cb
fatov <fat-ne@yandex.ru>
2025-03-16 16:43:10 +0300

    disable jit and parallel workers in pgbench

--- src/test/resources/pgbench/query_left_join_by_date.sql
+++ src/test/resources/pgbench/query_left_join_by_date.sql
@@ -2,6 +2,8 @@ SET SCHEMA 'test_insertion';
 
 -- \set id random(1027634, 1037634)
 
+SET jit = off;
+SET max_parallel_workers_per_gather = 0;
 select paymentdoc0_.id               as id1_4_,
        paymentdoc0_.account_id       as account10_4_,
        paymentdoc0_.amount           as amount2_4_,
@@ -17,4 +19,4 @@ select paymentdoc0_.id               as id1_4_,
 from payment_document paymentdoc0_
          left outer join active_transaction paymentdoc0_1_ on paymentdoc0_.id = paymentdoc0_1_.id
 where (paymentdoc0_1_.transaction_id is null)
-  AND paymentdoc0_.order_date = '2025-01-19';
+  AND paymentdoc0_.order_date = '2025-03-16';
